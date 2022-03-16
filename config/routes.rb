fb330a9b4a6e1ec42dc18dc65dddc0a06046790a
Wes-Isaac <yisehakmitiku@gmail.com>
2022-03-17 03:57:40 +0800

    Create migrations and setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'categories#index'
+
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :payments, only: [:index, :show, :new, :create]
+  end
+
 end
