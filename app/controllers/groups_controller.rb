b7f373ff7ad91fc954c8eded303b1daddeb3e6fc
Wes-Isaac <yisehakmitiku@gmail.com>
2022-03-17 21:41:51 +0800

    Create groups controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,44 @@
+class GroupController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = Group.new
+    @icons = icons
+  end
+
+  def create
+    group = current_user.groups.new(group_params)
+    respond_to do |format|
+      format.html do
+        if group.save
+          flash[:success] = 'Created New Group Succesfully!'
+          redirect_to groups_path
+        else
+          render :new, alert: 'Error occured!'
+        end
+      end
+    end
+  end
+
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to groups_path
+    flash[:success] = 'Group Deleted!'
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @payments = @group.payments
+  end
+
+  private
+
+  def group_params
+    params.require(:data).permit(:name, :icon)
+  end
+end
