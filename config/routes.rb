cad466e463661782849082d7e33a3c73369d331f
Wes-Isaac <yisehakmitiku@gmail.com>
2022-03-18 17:37:26 +0800

    Create navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'groups#index'
 
   resources :groups, only: [:index, :show, :new, :create, :destroy] do
-    resources :payments, only: [:index, :show, :new, :create]
+    resources :payments, only: [:index, :show, :new, :create, :destroy]
   end
 
 end
